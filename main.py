50d88fc2ab2ec13afc4d5710c688c973e0bdc8b7
jadon hoosier <hoosijad000@warren.k12.in.us>
2024-09-17 10:04:14 -0400

    2nd commit

--- main.py
+++ main.py
@@ -1,17 +1,69 @@
 import pygame
 
-#start pygame modules
+# Set display surface
 pygame.init()
 
-#set the display surface
+# Set display surface
 WINDOW_WIDTH = 1000
 WINDOW_HEIGHT = 400
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-pygame.display.set_caption("feed the dragon")
+pygame.display.set_caption("Feed The Dragon")
 
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+# Set FPS and clock
+FPS = 60
+clock = pygame.time.Clock()
 
-#the main game loop
+# Set game value:  CONSTANT_NAME, value
+''' 5 CONSTANTS
+PLAYER_STARTING_LIVES, 5
+PLAYER_VELOCITY, 10
+COIN_STARTING_VELOCITY, 10
+COIN_ACCELERATION, 0.5
+BUFFER_DISTANCE, 100
+'''
+PLAYER_STARTING_LIVES = 5
+# YOU do the remaining 4 CONSTANTS
+
+# Set Game Variables:  variable_name
+''' 3 variables
+score, 0
+player_lives, PLAYER_STARTING_LIVES
+coin_velocity, COIN_STARTING_VELOCITY
+'''
+score = 0
+# YOU do the remaining 2 variables
+
+# Set colors
+GREEN = (0, 255, 0)
+DARKGREEN = (10, 50, 10)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+# Set fonts
+font = pygame.font.Font('AttackGraffiti.ttf', 32)
+
+# Set Text for Score
+'''
+variable names:  score_text, score_rect
+render text: "Score: " + str(score)
+antialias: True
+color: GREEN
+background: DARKGREEN
+rect location: topleft = (10, 10)  
+'''
+score_text = font.render("Score: " + str(score), True, GREEN, DARKGREEN)
+score_rect = score_text.get_rect()
+score_rect.topleft = (10, 10)
+# Fill the display
+display_surface.fill(BLACK)
+
+display_surface.blit(score_text, score_rect)
+''' display_surface.blit(title_text, title_rect)
+display_surface.blit(lives_text, lives_rect)'''
+# Update display and tick the clock
+pygame.display.update()
+clock.tick(FPS)
+# The main game loop
 running = True
 while running:
     for event in pygame.event.get():
